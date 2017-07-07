408ee017dabbb67476ed94323af36395e858b88b
Daniela Kirsch <kirsch.daniela@gmail.com>
2017-07-07 13:18:40 -0400

    up

--- js/app.js
+++ js/app.js
@@ -24,8 +24,29 @@ $(document).ready(function(){
 		    }
 		});
 
-	
-	
+	if($( '.swipebox' ).length > 0) {
+		$( '.swipebox' ).swipebox();
+
+	}
+
+	$( '#gallery' ).click( function( e ) {
+		e.preventDefault();
+		$.swipebox( [
+			{ href:'images/day3.jpg', title:'Shichahai Shadow Art Hotel – Beijing' }, 
+			{ href:'images/hotel1.jpg', title:'Shichahai Shadow Art Hotel – Beijing' },
+
+			{ href:'images/imagecon2.jpg', title:'Shichahai Shadow Art Hotel – Beijing' },
+
+			{ href:'images/imagecon1.jpg', title:'Shichahai Shadow Art Hotel – Beijing' },
+
+			{ href:'images/imagecon3.jpg', title:'Shichahai Shadow Art Hotel – Beijing' },
+
+			{ href:'images/imagecon4.jpg', title:'Shichahai Shadow Art Hotel – Beijing' }
+
+
+
+		] );
+	} );
 
 
 });
